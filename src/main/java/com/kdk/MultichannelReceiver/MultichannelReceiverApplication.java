ffa92b9a3e979c15fbc91f9db9d555f5c8242dc1
Kamil Ochnik <ilmak@o2.pl>
2020-03-04 10:38:48 +0100

    initial commit ( java version 1.8 )

---
+++ src/main/java/com/kdk/MultichannelReceiver/MultichannelReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.kdk.MultichannelReceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultichannelReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultichannelReceiverApplication.class, args);
+	}
+
+}
