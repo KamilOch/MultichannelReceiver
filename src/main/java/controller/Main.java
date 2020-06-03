d2f4e5a27b7400ae69b9c3b6505be66dff102f9f
Kamil Ochnik <ilmak@o2.pl>
2020-06-03 14:21:49 +0200

    dodoalem Springa

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,47 @@
+package controller;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader= new FXMLLoader(
+					Main.class.getResource("/view/MainWindowReceiverView.fxml"));
+			
+			AnchorPane pane = loader.load();
+			primaryStage.setMinWidth(500.00);
+			primaryStage.setMinHeight(600.00);
+			Scene scene=new Scene(pane);
+
+//			SPRING!!
+			ConfigurableApplicationContext springContext;
+			springContext = SpringApplication.run(Main.class);
+			loader.setControllerFactory(springContext::getBean);
+
+
+			MainWindowController mainWindowController = loader.getController();
+			mainWindowController.setMain(this, primaryStage);
+			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
---
+++ src/main/java/dataPersist/RecordService.java
@@ -0,0 +1,4 @@
+package dataPersist;
+
+public class RecordService {
+}
