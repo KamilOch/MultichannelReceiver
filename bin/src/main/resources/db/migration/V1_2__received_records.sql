76927f6fa7db19ce59722361d4ace896ff9297e8
il <ilmak@o2.pl>
2020-06-18 19:47:41 +0200

    Dodaje jeszcze tabele sqla

---
+++ bin/src/main/resources/db/migration/V1_2__received_records.sql
@@ -0,0 +1,14 @@
+create sequence received_records_id_seq;
+
+create table received_records(
+
+id bigint primary key,
+
+frequency DOUBLE PRECISION not null,
+signal_level DOUBLE PRECISION not null,
+record_id bigint not null
+
+);
+
+alter table received_records
+add foreign key (record_id) references records (id) on delete cascade;
